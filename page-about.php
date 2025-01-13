dd073dac9bf96606163477745703466fafd4b5d7
Lawrence <lthomas@administrators-iMac.local>
2025-01-13 14:46:33 -0500

    updates to team member

--- page-about.php
+++ page-about.php
@@ -25,12 +25,14 @@
         // Load sub field value.
         $teamMemberImage = get_sub_field('team_member_image'); 
         $teamMemberName = get_sub_field('team_member_name');
+        $teamMemberPhone = get_sub_field('team_member_phone');
         ?>
        <li class="c-team-member">
           <figure class="c-team-member__image">
             <img src="<?php echo $teamMemberImage['url']; ?>" alt="<?php echo $teamMemberImage['alt']; ?>">
           </figure>
           <h4 class="c-team-member__name"><?php echo $teamMemberName; ?></h4>
+          <a class="c-team-member__phone" href="tel:<?php echo $teamMemberPhone; ?>"><?php echo $teamMemberPhone; ?></a>
        </li>
 
     <?php endwhile; ?>
