34bc72f71db3a4b5102ce19ce2fdc8e1344d7dff
Lawrence <lthomas@administrators-iMac.local>
2025-01-21 14:20:14 -0500

    Added Search icons

--- header.php
+++ header.php
@@ -78,7 +78,7 @@
 	<img class="mobile-menu-overlay__close-icon" src="<?php echo get_template_directory_uri(); ?>/img/icons/close-white.svg" alt="Close Icon"/>
 
 
-	<img class="search-btn" title ="Search Icon" src="<?php echo get_template_directory_uri(); ?>/img/icons/search.svg" alt="Search Icon">
+	<img class="search-btn" title ="Search Icon" src="<?php echo get_template_directory_uri(); ?>/img/icons/search-white.svg" alt="Search Icon">
 	</div>
   <nav class="mobile-navigation">
 			<?php
